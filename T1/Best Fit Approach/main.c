ea999acba9fe3ce194e945365b33bcbd7b3f76b0
Pedro Moraes <pedro_escalfoni@hotmail.com>
2018-05-05 00:42:27 -0300

    Update main.c

--- T1/Best Fit Approach/main.c
+++ T1/Best Fit Approach/main.c
@@ -1,55 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void binPacking(int *a, int tam, int n);
+#include "binpacking.h"
 
 int main(void) {
-	int n;
-	int *a, i = 0;
-	int tam;
+	int n;				/*numero de items*/
+	int i = 0;			/*indice*/
+	int j;				/*indice*/
+	int *array_pesos;	/*array de pesos*/
+	int cap;			/*capacidade*/
+	int countBin;		/*numero minimo de bins*/
 
-	FILE* arq;
+	FILE *arq;
+	Bin *bins;
+
 	arq = fopen("Falkenauer_t60_00.txt", "r");
 	if (arq == NULL) {
-		printf("Não existe arquivo.\n");
+		printf("NÃ£o existe arquivo.\n");
 		exit(1);
 	}
 
 	fscanf(arq, "%d", &n);
 
-	a = (int*)malloc(n * sizeof(int));
-	if (a == NULL) {
+	array_pesos = (int*)malloc(n * sizeof(int));
+	if (array_pesos == NULL) {
 		printf("Memoria insuficiente.\n");
 		exit(1);
 	}
 
-	fscanf(arq, "%d", &tam);
+	bins = (Bin*)malloc(n * sizeof(Bin));
+	if (bins == NULL) {
+		printf("Erro bins.\n");
+		exit(1);
+	}
 
-	while (fscanf(arq, "%d", &a[i]) != EOF) {
+	fscanf(arq, "%d", &cap);
+
+	while (fscanf(arq, "%d", &array_pesos[i]) != EOF) {
 		i++;
 	}
 
-	binPacking(a, tam, n);
+	countBin = binPacking(array_pesos, cap, n, bins);
+	bins = hillClimbing(bins, cap, countBin);
+	array_pesos = set_weight_array(bins, n, countBin);
+	countBin = binPacking(array_pesos, cap, n, bins);
+
+	printf("Numero minimo de bins: %d\n", countBin);
+
+	/*debug*/
+	/*for (i = 0; i < n; i++) {
+		for (j = 0; j < bins[i].qtditens; j++) {
+			printf("%d ", bins[i].pesos[j]);
+		}
+		printf("\n");
+	}*/
 
 	return 0;
-}
-
-void binPacking(int *a, int tam, int n) {
-	int contBin = 1, i;
-	int t = tam;
-
-	for (i = 0; i < n; i++) {
-		/* função heuristica */
-		if (t - *(a + i) > 0) {
-			t -= *(a + i);
-			continue;
-		}
-		else {
-			contBin++;
-			t = tam;
-			i--;
-		}
-	}
-
-	printf("Numero de bins necessarios: %d\n", contBin);
 }
